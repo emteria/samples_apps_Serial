3c0bac25ebe41acb25d4ceab9d73a376bfa1da6f
Stefan Schake <stschake@gmail.com>
2017-08-30 14:42:38 +0200

    Add license headers

--- app/src/main/java/com/emteria/serial/MainActivity.java
+++ app/src/main/java/com/emteria/serial/MainActivity.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright (C) 2017 emteria
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license.  See the LICENSE file for details.
+ */
+
 package com.emteria.serial;
 
 import android.app.Activity;
